74669a5d5c20546788d9d1cc82b0da10df3b5db3
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-09-06 22:47:28 +0600

    displayed categories dynamically in the left side from API

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const loadAllCategories = async() => {
 }
 
 const showAllCategories = (categories) => {
-    console.log(categories);
+    const categoryList = document.getElementById("category-list");
+    categories.forEach(category => {
+        categoryList.innerHTML += `
+            <li id="tree-${category.id}" class="px-3 py-2 cursor-pointer rounded duration-300 hover:bg-[#15803d] hover:text-white">${category.category_name}</li>
+        `;
+    });
 }
 
 loadAllCategories();
