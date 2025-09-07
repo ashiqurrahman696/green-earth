14c31d6dd39f183007b8f7758cb4de06c87cac16
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-09-07 08:45:29 +0600

    displayed plants by category in web page from API

--- script.js
+++ script.js
@@ -54,7 +54,32 @@ const loadPlantsByCategory = async(id) => {
     const url = `https://openapi.programming-hero.com/api/category/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.plants);
+    showPlantsByCategory(data.plants);
+}
+
+const showPlantsByCategory = (plants) => {
+    plantsContainer.innerHTML = "";
+    plants.forEach(plant => {
+        plantsContainer.innerHTML += `
+            <div class="card bg-base-100 w-full shadow-sm">
+                <figure class="px-4 pt-4">
+                    <img src="${plant.image}" alt="${plant.name}"
+                        class="rounded-xl w-full h-50 object-cover" />
+                </figure>
+                <div class="card-body pb-4">
+                    <h2 class="card-title">${plant.name}</h2>
+                    <p>${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <div class="rounded-full bg-[#dcfce7] text-[#15803d] py-1 px-3 font-semibold">${plant.category}</div>
+                        <div class="font-semibold">৳${plant.price}</div>
+                    </div>
+                </div>
+                <div class="pb-4 px-4">
+                    <button class="btn bg-[#15803d] btn-block rounded-full text-white">Add to Cart</button>
+                </div>
+            </div>
+        `;
+    });
 }
 
 loadAllCategories();
